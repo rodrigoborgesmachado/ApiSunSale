6b06e89d412057d35be7d50bfb7bb731dfa48a7b
Rodrigo Borges Machado <rodrigoborgesmachado@gmail.com>
2025-08-06 19:49:32 -0300

    Add PascalCase Usuarios table script

---
+++ ApiSunSale.Database/Tables/Usuarios.sql
@@ -0,0 +1,24 @@
+CREATE TABLE [dbo].[Usuarios](
+    [Id] [int] NOT NULL,
+    [Login] [varchar](50) NOT NULL,
+    [Pass] [varchar](12) NOT NULL,
+    [Nome] [varchar](50) NOT NULL,
+    [Email] [varchar](300) NULL,
+    [DataNascimento] [date] NULL,
+    [Admin] [char](1) NULL,
+    [Created] [datetime] NULL,
+    [Updated] [datetime] NULL,
+    [Instituicao] [nvarchar](255),
+PRIMARY KEY CLUSTERED
+(
+    [Id] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY],
+UNIQUE NONCLUSTERED
+(
+    [Login] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[Usuarios] ADD  DEFAULT ('0') FOR [Admin]
+GO
