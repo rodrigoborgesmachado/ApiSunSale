89bca1f2c99ecafd88220e19023ddc2c4020bb56
Barbara-RA <barbara.ramosalves94@gmail.com>
2025-10-27 18:45:39 -0300

    teste

--- ApiSunSale.Application/Interfaces/IComentariosquestoesAppService.cs
+++ ApiSunSale.Application/Interfaces/IComentariosquestoesAppService.cs
@@ -1,3 +1,4 @@
+using static ApiSunSale.Infrastructure.CrossCutting.Enums.Enums;
 using MainDTO = ApiSunSale.Application.DTO.ComentariosquestoesDTO;
 
 namespace ApiSunSale.Application.Interfaces
--- ApiSunSale.Presentation.Api/Controllers/ComentariosquestoesController.cs
+++ ApiSunSale.Presentation.Api/Controllers/ComentariosquestoesController.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Options;
 using IMainAppService = ApiSunSale.Application.Interfaces.IComentariosquestoesAppService;
 using MainDTO = ApiSunSale.Application.DTO.ComentariosquestoesDTO;
 using MainViewModel = ApiSunSale.Presentation.Model.ViewModels.ComentariosquestoesViewModel;
+using static ApiSunSale.Infrastructure.CrossCutting.Enums.Enums;
 
 namespace ApiSunSale.Presentation.Api.Controllers
 {
